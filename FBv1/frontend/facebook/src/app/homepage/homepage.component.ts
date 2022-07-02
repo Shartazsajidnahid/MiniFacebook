91405c23fcec72fea39ca62689c99d96197cd57f
Shartazsajidnahid <sajidnahid9658@gmail.com>
2022-07-02 18:48:49 +0600

    navbar, logout in homepage, posts filtered

--- FBv1/frontend/facebook/src/app/homepage/homepage.component.ts
+++ FBv1/frontend/facebook/src/app/homepage/homepage.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from '../post';
 import { PostService } from '../post.service';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-homepage',
@@ -10,14 +12,16 @@ import { PostService } from '../post.service';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(private PostService: PostService, private router: Router) { }
+  constructor(private PostService: PostService, private router: Router, private userservice : UserService) { }
 
   newpost = new Post();
+  currentUserId : String = "";
 
   postArr: Post[] = [];
 
   ngOnInit(): void {
     this.getPosts();
+    this.currentUserId = this.userservice.getToken();
   }
 
   // submitpost(){
@@ -29,19 +33,28 @@ export class HomepageComponent implements OnInit {
   getPosts(){
     this.PostService.getPost().subscribe(
       (res: any) => {
-         console.log('Got posts ' );
+        console.log('Got posts ' );
         console.log(res);
-        this.postArr = res as Post[];
+        this.postArr = res as Post[];this.filterPosts();
       }, (err: any) => {
         console.log('error');
       }
     );
+    
     // console.log(this.PostService.getPost());
-    
-
-    
   }
 
-  
+  logout():void{
+    this.userservice.currentuser = new User;
+    this.userservice.deleteToken();
+    this.router.navigate(['login']);
+  }
 
+  filterPosts(){
+      this.postArr = this.postArr.filter(
+          book => book.userid  != this.currentUserId);
+      
+      console.log("Filtered array");
+      console.log(this.postArr);
+  }
 }
